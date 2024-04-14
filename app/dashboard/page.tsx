074843518be4feab3e8c6987d0379f867363c903
ASAshry <ahmed.ashry.ds@gmail.com>
2024-04-14 20:38:39 +0200

    First commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+ 
+function Dashboard() {
+  return (
+    <div className='flex justify-center items-center h-screen gap-4'>
+        <h1>Select the Dashboard</h1>
+        <a className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" href='/dashboard/desktop'>Desktop</a>
+        <a className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" href='/dashboard/mobile'>Mobile</a>
+    </div>
+  )
+}
+
+export default Dashboard

88c33950799ef9ff44cee531e29c18cd606603ac
ASAshry <ahmed.ashry.ds@gmail.com>
2024-04-15 16:26:47 +0200

    edit mobile view

--- app/dashboard/mobile/page.tsx
+++ app/dashboard/mobile/page.tsx
@@ -1,8 +1,7 @@
 function Powerbi() {
   return (
       <div>
-          <iframe title="Hatchery Performance Dashboard.v1.3" width="375" height="1300" src="https://app.powerbi.com/view?r=eyJrIjoiZjg0OTg0ODYtNGJkNi00NWQ5LWFhZWUtMzQ4ZDNjZWNhMTFlIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9"></iframe>
-      </div>
+          <iframe title="Hatchery Performance Dashboard.v1.3" width="450" height="1650" src="https://app.powerbi.com/view?r=eyJrIjoiZjg0OTg0ODYtNGJkNi00NWQ5LWFhZWUtMzQ4ZDNjZWNhMTFlIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9" frameborder="0" allowFullScreen="true"></iframe>      </div>
   );
 }
 
